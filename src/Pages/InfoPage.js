b0fb27983a99886d492f03ced4c20ea772b74911
AbirDhar79 <77748693+AbirDhar79@users.noreply.github.com>
2025-04-18 00:01:44 +0530

    Update InfoPage.js for new chnages

--- src/Pages/InfoPage.js
+++ src/Pages/InfoPage.js
@@ -1,124 +1,4 @@
-// import { Stack, Typography, Box } from "@mui/material";
-// import LinkedInIcon from "@mui/icons-material/LinkedIn";
-// import WhatsAppIcon from "@mui/icons-material/WhatsApp";
-// import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
-// import TwitterIcon from "@mui/icons-material/Twitter";
-// import { GitHub } from "@mui/icons-material";
-// import Instagram from "@mui/icons-material/Instagram";
-// import { Link } from "react-router-dom";
-// import { Navbar } from ".";
-// import aboutCV from "../Utils/Images/aboutCV.jpg";
 
-// // the briefing of the website  nothing much only an overview of the resume@builder
-
-// export default function ButtonMUI() {
-//   return (
-//     <>
-//       <Navbar />
-//       <Stack p={{ xs: "15px", sm: "25px", md: "40px", lg: "60px " }}>
-//         <h2 className="template-header-title">Resume Builder</h2>
-//         <Stack
-//           className="midContainer"
-//           direction={{
-//             xs: "column-reverse",
-//             sm: "column-reverse",
-//             md: "column-reverse",
-//             lg: "row",
-//           }}
-//           spacing={{ xs: 1, sm: 2, md: 4 }}
-//           mt="20px"
-//         >
-//           <Typography
-//             sx={{
-//               fontSize: {
-//                 xs: "13px",
-//                 sm: "15px",
-//                 md: "17px",
-//                 lg: "19px",
-//               },
-//               paddingRight: {
-//                 xs: "15px",
-//                 sm: "18px",
-//                 lg: "25px",
-//               },
-//               textAlign: "justify",
-//             }}
-//           >
-//             You may create your resume quickly using the free online tool known as Resume Builder. With so many options at your disposal, you may quickly and professionally create a resume. Additional profile images can be uploaded to the resumes. You have the opportunity to see and download your resume after completing the fields. For local storage on your hard disc, click download. Your previously prepared resumes are available for updating, as well as deletion.
-
-//           </Typography>
-//           <Stack
-//             sx={{
-//               width: "30%",
-//               placeSelf: "center",
-//             }}
-//           >
-//             <img
-//               src={aboutCV}
-//               alt="img"
-//               // style={{width:"20%", height:"10%"}}
-//             />
-//           </Stack>
-//         </Stack>
-//         <Box mt="25px">
-//           <Typography
-//             sx={{
-//               fontSize: {
-//                 xs: "22px",
-//                 sm: "25px",
-//                 md: "27px",
-//                 lg: "30px",
-//               },
-//               fontWeight: "400",
-//               color: "dark",
-//             }}
-//           >
-//             Kindly Share in your circle
-//           </Typography>
-//           <Box className="icons">
-//             <Link to="https://www.instagram.com/dp_vasani56/">
-//               <Instagram
-//                 sx={{ fontSize: "40px", paddingLeft: "15px" }}
-//                 color="error"
-//               />
-//             </Link>
-
-//             <Link to="https://www.facebook.com/darshan.vasani.923">
-//               <FacebookOutlinedIcon
-//                 sx={{ fontSize: "40px", paddingLeft: "15px" }}
-//                 color="primary"
-//               />
-//             </Link>
-
-//             <Link to="https://www.linkedin.com/in/darshan-vasani-3299ba245/">
-//               <LinkedInIcon
-//                 sx={{ fontSize: "40px", paddingLeft: "15px" }}
-//                 color="primary"
-//               />
-//             </Link>
-//             <Link to="https://web.whatsapp.com">
-//               <WhatsAppIcon
-//                 sx={{ fontSize: "40px", paddingLeft: "15px" }}
-//                 color="success"
-//               />
-//             </Link>
-//             <Link to="https://twitter.com/vasanidarshan56">
-//               <TwitterIcon
-//                 sx={{ fontSize: "40px", paddingLeft: "15px" }}
-//                 color="info"
-//               />
-//             </Link>
-//             <Link to="https://github.com/dpvasani">
-//               <GitHub
-//                 sx={{ fontSize: "40px", paddingLeft: "15px", color: "black" }}
-//               />
-//             </Link>
-//           </Box>
-//         </Box>
-//       </Stack>
-//     </>
-//   );
-// }
 
 import { Stack, Typography, Box } from "@mui/material";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
@@ -196,19 +76,19 @@ const InfoPage = () => {
           </Typography>
           <SocialLinks>
             <Box className="icons">
-              <Link to="https://www.instagram.com/dp_vasani56/">
+              <Link to="https://www.instagram.com/dhar.abir/">
                 <Instagram
                   sx={{ fontSize: "40px", paddingLeft: "15px" }}
                   color="error"
                 />
               </Link>
-              <Link to="https://www.facebook.com/darshan.vasani.923">
+              <Link to="https://www.facebook.com/abir.dhar.549/">
                 <FacebookOutlinedIcon
                   sx={{ fontSize: "40px", paddingLeft: "15px" }}
                   color="primary"
                 />
               </Link>
-              <Link to="https://www.linkedin.com/in/darshan-vasani-3299ba245/">
+              <Link to="https://www.linkedin.com/in/abir-dhar-8b6747209/">
                 <LinkedInIcon
                   sx={{ fontSize: "40px", paddingLeft: "15px" }}
                   color="primary"
@@ -220,13 +100,13 @@ const InfoPage = () => {
                   color="success"
                 />
               </Link>
-              <Link to="https://twitter.com/vasanidarshan56">
+              <Link to="https://x.com/abirdhar79">
                 <TwitterIcon
                   sx={{ fontSize: "40px", paddingLeft: "15px" }}
                   color="info"
                 />
               </Link>
-              <Link to="https://github.com/dpvasani">
+              <Link to="https://github.com/AbirDhar79">
                 <GitHub
                   sx={{ fontSize: "40px", paddingLeft: "15px", color: "black" }}
                 />
